ebb50b8321e3b0d1b3689df7a14ab1da5f2de581
Luisa <luisa-s-1996@protonmail.com>
2025-11-10 14:29:55 +0100

    implement base entity

---
+++ Source/CoffeePot.Domain/Common/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using CoffeePot.Domain.Enumerations;
+
+namespace CoffeePot.Domain.Common;
+
+public abstract class BaseEntity
+{
+  public int Id { get; set; }
+  public DateTime CreationDate { get; set; }
+  public DateTime ChangeDate { get; set; }
+  public Status Status { get; set; }
+}
---
+++ Source/CoffeePot.Domain/Enumerations/Status.cs
@@ -0,0 +1,8 @@
+﻿namespace CoffeePot.Domain.Enumerations;
+
+public enum Status
+{
+  Deleted = -1,
+  Inactive = 0,
+  Active = 1
+}

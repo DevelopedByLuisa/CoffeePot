d8043f63b1069e66e05accc52d8587371d31753b
Luisa <luisa-s-1996@protonmail.com>
2026-01-08 11:01:00 +0100

    refactor project name

--- Source/CoffeePot.Web/Startup.cs
+++ Source/CoffeePot.Web/Startup.cs
@@ -2,7 +2,10 @@
 using System.IO;
 using System.Reflection;
 using System.Text.Json.Serialization;
+using CoffeePot.Infrastructure;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
@@ -26,6 +29,8 @@ public class Startup
       InitializeSwagger();
     }
 
+    InitializeDatabase();
+
     return _builder.Build();
   }
 
@@ -42,17 +47,17 @@ public class Startup
           {
             Email = "luisa-s-1996@protonmail.com",
             Name = "DevelopedByLuisa",
-            #pragma warning disable S1075
+#pragma warning disable S1075
             Url = new Uri("https://github.com/DevelopedByLuisa")
-            #pragma warning restore S1075
+#pragma warning restore S1075
           },
           Description = "A digital coffee fund for teams.",
           License = new OpenApiLicense
           {
             Name = "License",
-            #pragma warning disable S1075
+#pragma warning disable S1075
             Url = new Uri("https://github.com/DevelopedByLuisa/CoffeePot/blob/main/LICENSE")
-            #pragma warning restore S1075
+#pragma warning restore S1075
           },
           Title = "CoffeePot API",
           Version = "v1"
@@ -61,5 +66,14 @@ public class Startup
       var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
       options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
     });
+  }
+
+  private void InitializeDatabase()
+  {
+    var connectionString = _builder.Configuration.GetConnectionString("CoffeePotDb");
+    var serverVersion = ServerVersion.AutoDetect(connectionString);
+
+    _builder.Services.AddDbContext<ApplicationContext>(optionsBuilder =>
+      optionsBuilder.UseMySql(connectionString, serverVersion));
   }
 }

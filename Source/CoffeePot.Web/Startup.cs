3e980c546f3048a33e676e11ec10dbc8149101f9
Luisa <luisa-s-1996@protonmail.com>
2026-01-09 14:06:09 +0100

    refactor project

--- Source/CoffeePot.Web/Startup.cs
+++ Source/CoffeePot.Web/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
-namespace CoffeePot;
+namespace CoffeePot.Web;
 
 public class Startup
 {

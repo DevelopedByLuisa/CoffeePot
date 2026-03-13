952e9d4bd0ae084026ceb223ac28da1c7b60616a
Luisa <luisa-s-1996@protonmail.com>
2026-03-13 11:04:08 +0100

    update create script

--- Database/Create.sql
+++ Database/Create.sql
@@ -1,1 +1,24 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `coffee_pot` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `coffee_pot`;
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(55) NOT NULL,
+  `description` varchar(55) NOT NULL,
+  `unit_price` decimal(10,2) NOT NULL,
+  `creation_date` timestamp NOT NULL,
+  `change_date` timestamp NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
